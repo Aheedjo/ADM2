5891e5ef5691792fa8fc52dfb636977a358f678b
Maishanu Ahidjo <maishanu.ahijo@yahoo.com>
2022-10-12 17:11:49 +0100

    Completed the contact form, and added ability to send email.

--- src/pages/ContactMePage.jsx
+++ src/pages/ContactMePage.jsx
@@ -4,12 +4,7 @@ import Contact from '../components/Contact';
 const HireMePage = () => {
   return (
     <>
-        <div className='container'>
-            <h1 className='text-3xl text-center uppercase'>Contact Me</h1>
-            <p className='text-center'>Contact me, and I'll make sure to get back to you in less than 24 hours.</p>
-        </div>
         <Contact />
-        {/* <FAQ /> */}
     </>
   )
 }
